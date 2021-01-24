72152d2ad8dd0e7bb2953835c1c1c335b53f7f97
Bikash Katwal <bikashkatwal9@gmail.com>
2021-01-24 16:33:55 +1100

    updated api link

--- functions/createLinks.js
+++ functions/createLinks.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+require('dotenv').config();
 const {CREATE_LINKS} = require('./utils/linkQueries.js');
 const sendQuery = require('./utils/sendQuery');
 const formattedResponse = require('./utils/formattedResponse');
@@ -11,10 +13,10 @@ exports.handler = async(event)=>{
     const variables={name, url, description, archived:archived??false};
 
    try {
-       const {createLink: createdLink} = await sendQuery(CREATE_LINKS, variables);      
+       const {createLink: createdLink} = await sendQuery(CREATE_LINKS, variables);
        return formattedResponse( 200, createdLink);
    } catch (error) {
        console.log('ERRORRS',error);
-       return formattedResponse( 500,{err:'Oops!! something went wrong'} );      
+       return formattedResponse( 500,{err:'Oops!! something went wrong'} );
    }
 };
